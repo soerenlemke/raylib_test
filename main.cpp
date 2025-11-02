5c8d1648c8a4ec6fd4378cc035714945b017c3ee
soerenlemke <61249756+soerenlemke@users.noreply.github.com>
2025-11-02 15:02:53 +0100

    add player rectangle movement and rendering

--- main.cpp
+++ main.cpp
@@ -4,12 +4,14 @@
 int main() {
     constexpr int screenWidth = 800;
     constexpr int screenHeight = 450;
+    constexpr int playerHeight = 30;
+    constexpr int playerWidth = 100;
 
     InitWindow(screenWidth, screenHeight, "raylib-test");
 
-    Vector2 ballPosition = {
-        static_cast<float>(screenWidth)/2,
-        static_cast<float>(screenHeight)/2
+    Vector2 playerPosition = {
+        static_cast<float>(screenWidth)/2-static_cast<float>(playerWidth)/2,
+        static_cast<float>(screenHeight)-playerHeight
     };
 
     SetTargetFPS(60);
@@ -17,15 +19,19 @@ int main() {
     // main game loop
     while (!WindowShouldClose()) {
         // key inputs
-        if (IsKeyDown(KEY_D)) ballPosition.x += 2.0f;
-        if (IsKeyDown(KEY_A)) ballPosition.x -= 2.0f;
-        if (IsKeyDown(KEY_W)) ballPosition.y -= 2.0f;
-        if (IsKeyDown(KEY_S)) ballPosition.y += 2.0f;
+        if (IsKeyDown(KEY_D)) playerPosition.x += 3.0f;
+        if (IsKeyDown(KEY_A)) playerPosition.x -= 3.0f;
 
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawText("Hello, raylib!", 190, 200, 20, LIGHTGRAY);
-            DrawCircleV(ballPosition, 50, MAROON);
+
+            DrawRectangle(
+                static_cast<int>(playerPosition.x),
+                static_cast<int>(playerPosition.y),
+                playerWidth,
+                playerHeight,
+                BLACK);
+
         EndDrawing();
     }
 
